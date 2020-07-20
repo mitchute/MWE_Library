0291ffe9434a8ff4f705fbbe99608dc34a94078d
Matt Mitchell <matt.mitchell@nrel.gov>
2020-07-20 14:34:59 -0600

    messing around with setup

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
+import subprocess
+
 from setuptools import setup
+
+
+def get_version():
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip().decode("utf-8")
+
 
 setup(
     name='mwe_lib',
-    version='0.3',
-    packages=['mwe_lib'],
+    version=get_version(),
+    packages=['mwe_lib', ""],
     url='',
     license='',
     author='edwin',
     author_email='',
     description='MWE Library',
     package_data={
-        "mwe_lib": ["*.txt", "config.toml"],  # add any txt files in the mwe_lib package
+        "": ["config.toml"],
+        "mwe_lib": ["*.txt"],  # add any txt files in the mwe_lib package
     },
 )
