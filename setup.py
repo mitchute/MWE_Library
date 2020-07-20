7b7e1b1cdcf04f1dddd050c92e9922d5bf786545
Matt Mitchell <matt.mitchell@nrel.gov>
2020-07-20 15:58:10 -0600

    better library

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='mwe_lib',
-    version=0.5,
-    packages=['mwe_lib', "."],
+    version=0.6,
+    packages=['mwe_lib'],
     url='',
     license='',
     author='edwin',
     author_email='',
     description='MWE Library',
     package_data={
-        "mwe_lib": ["*"],
+        "mwe_lib": ["core/*", "resources/*", "config.toml", "runner.py"],
     },
 )
