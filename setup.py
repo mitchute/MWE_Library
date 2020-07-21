f600f75bc162b8bcdc89e9950ef32e99e3b052bf
Matt Mitchell <matt.mitchell@nrel.gov>
2020-07-21 11:50:42 -0600

    testing

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-import os
-
 from setuptools import setup
 
 
@@ -7,29 +5,6 @@ def read_lines(filename):
     with open(filename) as f_in:
         return f_in.readlines()
 
-
-basename = os.path.basename
-join = os.path.join
-dirname = os.path.dirname
-
-here = dirname(__file__)
-mwe_pkg_dir = join(here, "mwe_lib")
-
-include_file_exts = [".py", ".rb", ".csv"]
-exclude_files = ["dont-include.xml"]
-
-lib_files = []
-
-for root, dirs, files in os.walk(mwe_pkg_dir):
-    for f in files:
-        f_name = os.path.basename(f)
-        ext = os.path.splitext(f)[1]
-        if ext in include_file_exts and f_name not in exclude_files:
-            full_path = join(root, f)
-            f_path = full_path.split(here)[1]
-            lib_files.append(f_path)
-
-print(lib_files)
 
 setup(
     name="mwe_lib",
