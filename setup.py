dd13e30a821f546600630bfb7ee009ae88463cf6
Matt Mitchell <matt.mitchell@nrel.gov>
2020-07-21 08:35:11 -0600

    more testing

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ def read_lines(filename):
 
 
 setup(
-    name='mwe_lib',
-    version=0.7,
-    packages=['mwe_lib'],
-    url='',
-    license='',
-    author='edwin',
-    author_email='',
-    description='MWE Library',
-    package_data={
-        "mwe_lib": ["core/*", "resources/*", "config.toml", "runner.py"],
-    },
+    name="mwe_lib",
+    version=0.8,
+    packages=["mwe_lib"],
+    url="",
+    license="",
+    author="edwin",
+    author_email="",
+    description="MWE Library",
+    package_dir={"mwe_lib": "mwe_lib"},
+    package_data={"": ["config2.toml"]},
+    include_package_data=True,
     install_requires=read_lines("requirements.txt"),
 )
